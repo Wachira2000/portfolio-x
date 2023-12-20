71c1a92e54719beb773656c0545aee87065c0923
Wachira2000 <lewiswachira996@gmail.com>
2023-12-20 22:10:45 +0300

    Changes

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -26,9 +26,9 @@ const HeroSection = () => {
                 1000,
                 "Web Developer",
                 1000,
-                "Mobile Developer",
+                "App Developer",
                 1000,
-                "Salesforce Developer",
+                "UI/UX Designer",
                 1000,
               ]}
               wrapper="span"
@@ -37,8 +37,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+          I&apos;m a talented and accomplished software engineer who focuses on creating and maintaining high-quality software.
           </p>
           <div>
             <Link
@@ -48,7 +47,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="https://drive.google.com/file/d/14QAK_lehp4w7dGSTcXSvhsMXDimjZdHZ/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
@@ -65,7 +64,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/Sutii.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
